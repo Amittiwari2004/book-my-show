d14ddcc395c7cbf31678d5be44533828888ce707
Amit Tiwari <amittiwari00512@gmail.com>
2026-09-14 14:46:42 +0530

    test: verify development ci cd

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
       }}
     >
       <h1 style={{ marginBottom: "10px" }}>
-        Book My Show
+       Book My Show - CI/CD
       </h1>
 
       <p style={{ color: "#666", marginBottom: "30px" }}>
