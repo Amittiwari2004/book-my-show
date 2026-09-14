027ad7d40c3f51b1ab000a5398c36dce07f03a95
Amit Tiwari <amittiwari00512@gmail.com>
2026-09-14 23:27:38 +0530

    fix: update title in Home component from "CI/CD AUTOMATED" to "Automate"

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
       }}
     >
       <h1 style={{ marginBottom: "10px" }}>
-     Book My Show - CI/CD AUTOMATED
+     Book My Show - Automate
       </h1>
 
       <p style={{ color: "#666", marginBottom: "30px" }}>
