a4563719f020c301de2f73470cf6b1911dfb8335
Amit Tiwari <amittiwari00512@gmail.com>
2026-09-14 23:17:33 +0530

    fix: update title from "MVP" to "CI/CD AUTOMATED" in Home component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
       }}
     >
       <h1 style={{ marginBottom: "10px" }}>
-       Book My Show - MVP
+     Book My Show - CI/CD AUTOMATED
       </h1>
 
       <p style={{ color: "#666", marginBottom: "30px" }}>
